ce498d74b40c648ed7ab50d4b200f3f0bb5a62ba
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-09-23 02:20:35 +0530

    update 7 & add 8

--- sem-3/OOP-Lab/7.cpp
+++ sem-3/OOP-Lab/7.cpp
@@ -17,21 +17,31 @@ public:
 
     void get_Data()
     {
-        cout << "Name: " << name << ", Roll: " << roll << endl;
+        cout << "Name: " << name << "\nRoll: " << roll << endl;
     }
 };
 
 int main()
 {
     Student student1, student2;
+    string name1, name2;
+    int roll1, roll2;
+    cout << "Student 1 Name: ";
+    cin >> name1;
+    cout << "Student 1 Roll: ";
+    cin >> roll1;
+    cout << "Student 2 Name: ";
+    cin >> name2;
+    cout << "Student 2 Roll: ";
+    cin >> roll2;
 
-    student1.set_Data("Alice", 1);
-    student2.set_Data("Bob", 2);
+    student1.set_Data(name1, roll1);
+    student2.set_Data(name2, roll2);
 
-    cout << "Student 1: ";
+    cout << "\nStudent 1: \n";
     student1.get_Data();
 
-    cout << "Student 2: ";
+    cout << "\nStudent 2: \n";
     student2.get_Data();
 
     return 0;
--- sem-3/OOP-Lab/8.cpp
+++ sem-3/OOP-Lab/8.cpp
@@ -1,1 +1,52 @@
-// To-Do
+#include <iostream>
+#define MAX 3
+using namespace std;
+
+class Student
+{
+private:
+    string sname;
+    int marks[MAX];
+    int total = 0;
+    int tmax;
+
+public:
+    void assign()
+    {
+        cout << "Enter Name: ";
+        cin >> sname;
+        for (int i = 0; i < MAX; i++)
+        {
+            cout << "Enter Marks In Subject " << i + 1 << ": ";
+            cin >> marks[i];
+        }
+    }
+    void compute()
+    {
+        for (int i = 0; i < MAX; i++)
+        {
+            total += marks[i];
+        }
+        tmax = total / MAX;
+    }
+    void display()
+    {
+        cout << "\nName: " << sname;
+        cout << "\nMarks: ";
+        for (int i = 0; i < MAX; i++)
+        {
+            cout << marks[i] << " ";
+        }
+        cout << "\nTotal: " << total;
+        cout << "\nAverage: " << tmax;
+    }
+};
+
+int main()
+{
+    Student s;
+    s.assign();
+    s.compute();
+    s.display();
+    return 0;
+}
